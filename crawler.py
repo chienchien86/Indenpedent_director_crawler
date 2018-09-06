2e505b2066441cc43cdeaff73e7c5993c87f4f61
王盈蒨 <wyingqian365@gmail.com>
2018-09-06 23:28:26 +0800

    改用multiprocess的方法

--- crawler.py
+++ crawler.py
@@ -1,15 +1,25 @@
 # -*- coding: utf-8 -*-
 from requests_html import HTML
-import requests
 from pyquery import PyQuery as pq
-# import psycopg2
+import requests
+import psycopg2
+from multiprocessing import Pool
+import configparser
+import os
 
-def get_metadata_from(url):
+CONFIG_FILE = os.environ.get('CONFIG_FILE', 'config.ini')
+CONFIG_FILE_LOCAL = os.environ.get('CONFIG_FILE_LOCAL', 'config.local.ini')
+config = configparser.ConfigParser()
+config.read(CONFIG_FILE, "utf-8")
 
-    def fetch(url):
-        play_load = {'encodeURIComponen': 1,'step': 1,'firstin': 1,'off': 1,'TYPEK': 'sii',}
-        response = requests.post(url, data=play_load)
-        return response
+def get_config_value(Section, Key):
+    return config.get(Section, Key)
+
+def fetch(url,play_load):
+    response = requests.post(url, data=play_load)
+    return response.text
+
+def get_metadata_from(resp):
 
     def parse_article_entries(doc):
         html = HTML(html=doc)
@@ -17,51 +27,47 @@ def get_metadata_from(url):
         return post_entries
 
     def entry_groups(post_entries):
+        ##分類邏輯  每個<table class="hasBorder">下有很多個tr 同樣class屬性的為同一個人的資料
+        # [l] [cccccccc] [llll] [ccc] [ll ]
 
         i = 0
         entry_group = []
 
-        for entry in post_entries[:50]:
+        for entry in post_entries:
 
             if i == 0:
-               previous = pq(entry.html).hasClass('lColor')
-               entry_group.append([entry])
-               i = i +1
-               continue
+                previous = pq(entry.html).hasClass('lColor')
+                entry_group.append([entry])
+                i = i + 1
+                continue
 
             if previous == pq(entry.html).hasClass('lColor'):
-               entry_group[-1].extend([entry])
-               i = i +1
-               continue
+                entry_group[-1].extend([entry])
+                i = i + 1
+                continue
 
             else:
-               previous = pq(entry.html).hasClass('lColor')
-               entry_group.append([entry])
-               i = i +1
-               continue
+                previous = pq(entry.html).hasClass('lColor')
+                entry_group.append([entry])
+                i = i + 1
+                continue
 
-        return entry_group
-
+            return entry_group
     def entry_group_meta(entry_group):
+
 
         sequence_number = pq(entry_group[0].html).find('td').eq(0).text()
         stock_symbol = pq(entry_group[0].html).find('td').eq(1).text()
-        company_name = pq(entry_group[0].html).find('td').eq(2).text()
+        Company_Name = pq(entry_group[0].html).find('td').eq(2).text()
         job_title = pq(entry_group[0].html).find('td').eq(3).text()
         naturalperson_name = pq(entry_group[0].html).find('td').eq(4).text()
         Date_of_inauguration = pq(entry_group[0].html).find('td').eq(5).text()
-
-        current_jobs = []
-        core_experiences = []
         adjunct_companys = []
         adjunct_job_tiltes = []
 
+
         for j in range(len(entry_group)):
-
-            if j==0:
-
-                current_jobs.append(pq(entry_group[j].html).find('td.textleft[rowspan]').eq(0))
-                core_experiences.append(pq(entry_group[j].html).find('td.textleft[rowspan]').eq(1))
+            if j == 0:
                 adjunct_companys.append(pq(entry_group[j].html).find('td.textleft').eq(2).text())
                 adjunct_job_tiltes.append(pq(entry_group[j].html).find('td.textleft').eq(3).text())
 
@@ -69,53 +75,38 @@ def get_metadata_from(url):
                 adjunct_companys.append(pq(entry_group[j].html).find('td').eq(0).text())
                 adjunct_job_tiltes.append(pq(entry_group[j].html).find('td').eq(1).text())
 
-
-
-        for adjunct_company, adjunct_job_tilte in zip(adjunct_companys,adjunct_job_tiltes):
+        results = []
+        for adjunct_company, adjunct_job_tilte in zip(adjunct_companys, adjunct_job_tiltes):
             result = {}
             result['序號'] = sequence_number
             result['公司代號'] = stock_symbol
-            result['公司名稱'] = company_name
+            result['公司名稱'] = Company_Name
             result['職位'] = job_title
             result['姓名'] = naturalperson_name
             result['就任日期'] = Date_of_inauguration
             result['兼任公司'] = adjunct_company
             result['兼任公司職稱'] = adjunct_job_tilte
+            results.append(result)
 
-            print(result)
+        return results
 
-        for current_job in current_jobs:
-            director_current_job = {}
-            director_current_job['序號'] = sequence_number
-            director_current_job['公司代號'] = stock_symbol
-            director_current_job['公司名稱'] = company_name
-            director_current_job['職位'] = job_title
-            director_current_job['姓名'] = naturalperson_name
-            director_current_job['就任日期'] = Date_of_inauguration
-            director_current_job['主要現職'] = current_job.text()
-
-            print(director_current_job)
-
-        for core_experience in core_experiences:
-            director_core_experience = {}
-            director_core_experience['序號'] = sequence_number
-            director_core_experience['公司代號'] = stock_symbol
-            director_core_experience['公司名稱'] = company_name
-            director_core_experience['職位'] = job_title
-            director_core_experience['姓名'] = naturalperson_name
-            director_core_experience['主要經歷'] = core_experience.text()
-
-            print(director_core_experience)
-
-
-
-    resp = fetch(url)
-    resp.encoding = "utf-8"
-    post_entries = parse_article_entries(resp.text)
+    post_entries = parse_article_entries(resp)
     entry_group = entry_groups(post_entries)
+    Result_group = []
     for entry in entry_group:
-        entry_group_meta(entry)
-
+        Results = entry_group_meta(entry)
+        Result_group += Results
+    return Result_group
 
 if __name__ == '__main__':
-   get_metadata_from('http://mops.twse.com.tw/mops/web/t93sc01_1')
+    # 獨立董事 的市場別有上市、上櫃、興櫃、公開發行分別post不同的參數
+    categroy_list = ['sii', 'otc', 'rotc', 'pub']
+    post_links = []
+    Result_group = []
+    for i in range(len(categroy_list)):
+        play_load = {'encodeURIComponen': 1, 'step': 1, 'firstin': 1, 'off': 1, 'TYPEK': categroy_list[i]}
+        Result = fetch('http://mops.twse.com.tw/mops/web/t93sc01_1', play_load)
+        Result_group.append(Result)
+
+    with Pool(processes=4) as pool:
+        contents = pool.map(get_metadata_from, Result_group)
